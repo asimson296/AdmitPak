3cad5c29978e39d59c8153612df51f2630c878c4
ranakali <ranakali@admitpak.local>
2026-09-25 03:02:03 -0400

    Use Render PORT env variable in production

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const auth = require("./auth");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Parse JSON requests
 app.use(express.json());
